77267169f14465a02fc999d9c17f68ee6866bfa0
Arnav Gupta <arnav@codingblocks.com>
2018-09-19 11:58:22 +0530

    day 1: part 1
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

---
+++ Day01/js_basics/script.js
@@ -0,0 +1,12 @@
+console.log("hello")
+
+// strongly typed vs weakly typed
+// statically typed vs dynamically typed
+// there are no datatypes for variables
+let x = 10
+console.log(x)
+console.log(typeof x)
+
+x = "a string"
+console.log(x)
+console.log(typeof x)
