6a8cf92dc1902a1bdfcd54ca11efce9b04d7faeb
Arnav Gupta <arnav@codingblocks.com>
2018-10-24 12:21:14 +0530

    params and query
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day10/express-sample/server.js
+++ Day10/express-sample/server.js
@@ -2,8 +2,33 @@ const express = require('express')
 
 const server = express()
 
+let greetings = [
+  'Good Morning',
+  'How are you ?',
+  'What a great day!'
+]
+
 server.get('/', (req, res) => {
   res.send('Hello World')
+})
+
+server.get('/greet', (req, res) => {
+  res.send('Goodmorning! ')
+})
+
+server.get('/greet/:id', (req, res) => {
+  if (isNaN(parseInt(req.params.id))) {
+    res.send('Wrong greeting id')
+  }
+  res.send(greetings[req.params['id']])
+})
+
+server.get('/hello', (req, res) => {
+  console.log(req.query)
+  if (!req.query.name) {
+    req.query.name = 'Guest'
+  }
+  res.send('Hello ' + req.query.name)
 })
 
 
