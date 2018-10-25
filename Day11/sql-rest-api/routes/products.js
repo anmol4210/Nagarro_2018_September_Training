17a3ce22c5cee88438030ac0dc3e37b447a86d4b
Arnav Gupta <arnav@codingblocks.com>
2018-10-25 16:02:52 +0530

    query filters
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- Day11/sql-rest-api/routes/products.js
+++ Day11/sql-rest-api/routes/products.js
@@ -1,13 +1,48 @@
 const { Router } = require('express')
+const { Op } = require('sequelize')
 const { Product, Vendor } = require('../db/index')
 
 
 const route = Router()
 
 route.get('/', async (req, res) => {
+  let whereClause = []
+  for (let key of Object.keys(req.query)) {
+
+    switch(key) {
+      case 'minQty':
+        whereClause.push({
+          quantity: {[Op.gt]: req.query.minQty}
+        })
+        break;
+      case 'maxQty':
+        whereClause.push({
+          quantity: {[Op.lt]: req.query.maxQty}
+        })
+        break;
+      case 'minPrice':
+        whereClause.push({
+          price: {[Op.gt]: req.query.minPrice}
+        })
+        break;
+      case 'maxPrice':
+        whereClause.push({
+          price: {[Op.lt]: req.query.maxPrice}
+        })
+        break;
+    }
+  }
+
+  console.log('=============')
+  console.log(whereClause)
+  console.log('=============')
+  console.log('=============')
 
   const products = await Product.findAll({
-    include: [Vendor]
+    include: [Vendor],
+    where: {
+      [Op.and]: whereClause
+    }
   })
   res.status(200).json(products)
 
